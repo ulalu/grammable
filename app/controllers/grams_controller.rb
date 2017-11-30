2c1ffa104a12eb0237d1f55170daa9cdd4ccacf7
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-11-30 23:15:59 +0000

    made create paths for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,6 +3,18 @@ class GramsController < ApplicationController
         @gram = Gram.new
     end
     
+    def create
+        @gram = Gram.create(gram_params)
+        redirect_to root_path
+    end
+    
     def index
     end
+    
+    private
+    
+    def gram_params
+        params.require(:gram).permit(:message)
+    end
+    
 end
