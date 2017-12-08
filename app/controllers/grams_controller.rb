c80b460212b1f0f275b237b8afaaee40616f0810
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-12-08 21:14:38 +0000

    added destroy action for grams and associated tests

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -40,6 +40,18 @@ class GramsController < ApplicationController
       end
     end
     
+    def destroy
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        
+        @gram.destroy
+        
+        if @gram.valid?
+            redirect_to root_path
+        end
+        
+    end
+    
     
     private
     
