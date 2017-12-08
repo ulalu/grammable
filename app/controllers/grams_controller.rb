9842551e79809782110f2cc8198ab48758d93a3c
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-12-08 22:25:42 +0000

    specified that user changes could only affect associated grams, and tested

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def new
         @gram = Gram.new
@@ -25,30 +25,32 @@ class GramsController < ApplicationController
     def edit
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
+        
     end
     
     def update
-      @gram = Gram.find_by_id(params[:id])
-      return render_not_found if @gram.blank?
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user !=current_user
     
-      @gram.update_attributes(gram_params)
+        @gram.update_attributes(gram_params)
     
-      if @gram.valid?
-        redirect_to root_path
-      else
-        return render :edit, status: :unprocessable_entity
-      end
+        if @gram.valid?
+            redirect_to root_path
+        else
+            return render :edit, status: :unprocessable_entity
+        end
     end
     
     def destroy
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user !=current_user
         
         @gram.destroy
         
-        if @gram.valid?
-            redirect_to root_path
-        end
+        redirect_to root_path
         
     end
     
@@ -59,8 +61,8 @@ class GramsController < ApplicationController
         params.require(:gram).permit(:message)
     end
     
-    def render_not_found
-        render plain: 'Not Found', status: :not_found
+    def render_not_found(status=:not_found)
+        render plain: "#{status.to_s.titleize}", status: status
     end
     
 end
